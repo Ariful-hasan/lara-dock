8a4ddba87ba52cdbe03e864e4795eb669c233df8
Ariful Hasan <arifhasan.mbstu@gmail.com>
2025-12-28 21:17:35 +0100

    update entrypoin for test running

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,35 +14,37 @@ else
     php artisan route:clear
 fi
 
-# 2. Wait for Database
-echo "Waiting for database connection..."
-until php -r "try { new PDO(getenv('DB_CONNECTION') . ':host=' . getenv('DB_HOST') . ';port=' . getenv('DB_PORT') . ';dbname=' . getenv('DB_DATABASE'), getenv('DB_USERNAME'), getenv('DB_PASSWORD')); exit(0); } catch (Exception \$e) { exit(1); }"; do
-  echo "Database is unavailable - sleeping..."
-  sleep 2
-done
-echo "Database is ready!"
+
+if [ "$APP_ENV" != "testing" ]; then
+    # 2. Wait for Database
+    echo "Waiting for database connection..."
+    until php -r "try { new PDO(getenv('DB_CONNECTION') . ':host=' . getenv('DB_HOST') . ';port=' . getenv('DB_PORT') . ';dbname=' . getenv('DB_DATABASE'), getenv('DB_USERNAME'), getenv('DB_PASSWORD')); exit(0); } catch (Exception \$e) { exit(1); }"; do
+    echo "Database is unavailable - sleeping..."
+    sleep 2
+    done
+    echo "Database is ready!"
 
 
-# 3. Wait for Redis (Recommended since you use phpredis)
-echo "Waiting for Redis connection..."
-until php -r "
-    \$redis = new Redis();
-    try {
-        \$redis->connect(getenv('REDIS_HOST'), (int)getenv('REDIS_PORT'));
-        if (getenv('REDIS_PASSWORD')) {
-            \$redis->auth(getenv('REDIS_PASSWORD'));
+    # 3. Wait for Redis (Recommended since you use phpredis)
+    echo "Waiting for Redis connection..."
+    until php -r "
+        \$redis = new Redis();
+        try {
+            \$redis->connect(getenv('REDIS_HOST'), (int)getenv('REDIS_PORT'));
+            if (getenv('REDIS_PASSWORD')) {
+                \$redis->auth(getenv('REDIS_PASSWORD'));
+            }
+            \$redis->ping();
+            exit(0);
+        } catch (Exception \$e) {
+            exit(1);
         }
-        \$redis->ping();
-        exit(0);
-    } catch (Exception \$e) {
-        exit(1);
-    }
-"; do
-  echo "Redis is unavailable - sleeping..."
-  sleep 2
-done
-echo "Redis is ready!"
-
+    "; do
+    echo "Redis is unavailable - sleeping..."
+    sleep 2
+    done
+    echo "Redis is ready!"
+fi
 
 echo "Running migrations..."
 php artisan migrate --force
